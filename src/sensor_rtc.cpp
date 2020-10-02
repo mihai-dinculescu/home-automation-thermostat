1ca02cbfbed5efc3ef36ba6277c9a595998bddad
Mihai Dinculescu <mihai.dinculescu@outlook.com>
2020-10-02 21:27:48 +0100

    Remove clock alarm

--- src/sensor_rtc.cpp
+++ src/sensor_rtc.cpp
@@ -3,28 +3,25 @@
 
 bool SensorRTC::Setup()
 {
-    if (!_sensor.begin()) {
+    if (!_sensor.begin())
+    {
         return false;
     }
 
-    if (_sensor.lostPower() || _sensor.alarmFired(1)) {
-        if (_sensor.lostPower()) {
-            LOGLNT("RTC lost power.");
-        } else {
-            LOGLNT("RTC refresh alarm fired.");
-        }
+    if (_sensor.lostPower())
+    {
+        LOGLNT("RTC lost power.");
 
-        if (!board.SetupTime()) {
+        if (!board.SetupTime())
+        {
             return false;
         }
 
         // set sensor datetime
         _sensor.adjust(DateTime(board.GetTimestamp() / 1000));
-
-        // set alarm for when next to update the time from NTP
-        _sensor.clearAlarm(1);
-        _sensor.setAlarm1(_sensor.now() + TimeSpan(1, 0, 0, 0), Ds3231Alarm1Mode::DS3231_A1_Minute);
-    } else {
+    }
+    else
+    {
         LOGT("Setting time from RTC.");
 
         int epoch_time = _sensor.now().unixtime();
